32238d8c2d9c009a95f76a59638ba63d38e351f5
Renato Souza <renatoufu@gmail.com>
2023-08-11 13:32:29 -0400

    Adds an implementation for a new problem: SuperReducedString

--- src/test/java/prepare/algorithms/SuperReducedString.java
+++ src/test/java/prepare/algorithms/SuperReducedString.java
@@ -1,2 +1,38 @@
-package prepare.algorithms;public class SuperReducedString {
+package prepare.algorithms;
+
+import java.util.Stack;
+
+public class SuperReducedString {
+
+    /**
+     * <a href="https://www.hackerrank.com/challenges/reduced-string">Super Reduced String</a>
+     * Complete the 'superReducedString' function below.
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+    public static String superReducedString(String s) {
+        Stack<Character> readLetters = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character currentChar = s.charAt(i);
+            if (!readLetters.isEmpty()) {
+                if (currentChar.equals(readLetters.peek())) {
+                    readLetters.pop();
+                } else {
+                    readLetters.push(currentChar);
+                }
+            } else {
+                readLetters.push(currentChar);
+            }
+        }
+        if (readLetters.isEmpty()) {
+            return "Empty String";
+        } else {
+            StringBuilder result = new StringBuilder();
+            while (!readLetters.isEmpty()) {
+                result.insert(0, readLetters.pop());
+            }
+            return result.toString();
+        }
+    }
+
 }
--- src/test/java/prepare/algorithms/SuperReducedStringTest.java
+++ src/test/java/prepare/algorithms/SuperReducedStringTest.java
@@ -1,4 +1,32 @@
-import static org.junit.jupiter.api.Assertions.*;
+package prepare.algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SuperReducedStringTest {
-  
+
+    @Test
+    void superReducedStringTestCase0() {
+        String input = "aaabccddd";
+        String expectedOutput = "abd";
+
+        assertEquals(expectedOutput, SuperReducedString.superReducedString(input));
+    }
+
+    @Test
+    void superReducedStringTestCase1() {
+        String input = "aa";
+        String expectedOutput = "Empty String";
+
+        assertEquals(expectedOutput, SuperReducedString.superReducedString(input));
+    }
+
+    @Test
+    void superReducedStringTestCase2() {
+        String input = "baab";
+        String expectedOutput = "Empty String";
+
+        assertEquals(expectedOutput, SuperReducedString.superReducedString(input));
+    }
 }
